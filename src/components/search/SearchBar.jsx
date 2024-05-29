6a735b939fdc9ee0e6fde98d97edc09b4d97626a
RohanSangle <rohan.sangle02@gmail.com>
2024-05-29 15:59:58 +0530

    searchbar added

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './searchbar.css'
+
+import iconSearch from '../../assets/icon-search.svg'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='search'>
+      <img src={iconSearch} alt=''/>
+      <input 
+        // value={value} 
+        // onChange={handleChange} 
+        placeholder='Search for movies or TV series' 
+        type='search' 
+        name='search' 
+        id='search'
+      />
+    </div>
   )
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './main.css'
 
 import MenuBar from '../../components/menuBar/MenuBar.jsx'
+import SearchBar from '../../components/search/SearchBar.jsx'
 
 const Main = () => {
   return (
     <>
       <MenuBar/>
+      <SearchBar/>
     </>
   )
 }
