fe50c9973a2d5688ea0feb363e422ddde51596a8
RohanSangle <rohan.sangle02@gmail.com>
2024-05-29 18:51:46 +0530

    basic card made, next step to map it with data.json

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './card.css'
 
+import beyondearth from '../../assets/thumbnails/beyond-earth/regular/large.jpg'
+import play from '../../assets/icon-play.svg'
+import bookmarkempty from '../../assets/icon-bookmark-empty.svg'
+
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img className='bgimg' src={beyondearth} alt='' />
+
+      <div className='playbutton'>
+        <img className='play' src={play} alt=''/>
+        <h4>Play</h4>
+      </div>
+
+      <div className='bookmark-shade'>
+        <img className='bookmarkbutton' src={bookmarkempty} alt=''/>
+      </div>
+
+      <div className='subtitle'>
+        <h5>2019</h5>
+        <p>.</p>
+        <img src='' alt=''/>
+        <h5>Movie</h5>
+        <p>.</p>
+        <h5>PG</h5>
+      </div>
+
+      <h3 className='title'>Beyond Earth</h3>
+
+    </div>
   )
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,12 +3,18 @@ import './main.css'
 
 import MenuBar from '../../components/menuBar/MenuBar.jsx'
 import SearchBar from '../../components/search/SearchBar.jsx'
+import Card from '../../components/card/Card.jsx'
 
 const Main = () => {
   return (
     <>
       <MenuBar/>
       <SearchBar/>
+      {/* <h2>Trending</h2> */}
+      <h2>Recommended for you</h2>
+      <div className='card-background'>
+        <Card/>
+      </div>
     </>
   )
 }
